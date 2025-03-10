77008b805855e25469539c66dfacfa5bf968c78e
sashwat <sashwatbarthwal@gmail.com>
2025-03-10 19:07:18 +0530

    implement login api

--- routes/authentication.js
+++ routes/authentication.js
@@ -9,7 +9,30 @@ const bcrypt=require('bcryptjs');
 router.get('/',(req,res)=>{
     res.send("Hello from sashwat");
 });
-
+router.post('/login',(req,res)=>{
+    const {email,password}=req.body;//destructuring
+    if(!email || !password){
+        return res.status(400).json({error:"one or more required fields are empty"});//i dont want to continue furthur after encountering this error
+    }
+    UserModel.findOne({email:email})
+    .then((dbUser)=>{
+        if(!dbUser){//user not found
+            return res.status(400).json({error:"User not found"});//i dont want to continue furthur after encountering this error
+        }
+        bcrypt.compare(password,dbUser.password)
+        .then((didMatch)=>{
+            if(didMatch){
+                return res.status(200).json({result:"successful Log IN"});//check what happens if i remove return here
+            }
+            else{
+                return res.status(400).json({error:"Invalid Credentials"});//i dont want to continue furthur after encountering this error
+            }
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+});
 router.post('/register',(req,res)=>{
     console.log(req.body);
     const {fullName,email,password}=req.body;//object destructuring
