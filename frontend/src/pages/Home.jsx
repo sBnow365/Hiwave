21fae8cc3a89d93293831374eff033ef1788aff0
MohdShaad3187 <mohdshaad3187@gmail.com>
2025-03-23 17:46:13 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 
-function Home() {
+function Home() {//home page
   return (
     <div className='home-container'>
       <div className='card home-card'>
