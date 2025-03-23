53556152b53d3c49f03e3455e3f16a2654273069
sashwat <sashwatbarthwal@gmail.com>
2025-03-23 16:33:46 +0530

    working ini backend of profile picture

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,19 +1,27 @@
-const mongoose=require('mongoose')// to import the modules
-
-const userSchema=new mongoose.Schema({
-    fullName:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    followers:[{type:ObjectId,ref:"UserModel"}],
-    following:[{type:ObjectId,ref:"UserModel"}]
-})
 mongoose.model("UserModel",userSchema);
+
+const mongoose = require('mongoose'); // Import mongoose
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicUrl :{
+        type:String,
+        default :"https://res-console.cloudinary.com/dku7k2gnt/thumbnails/v1/image/upload/v1742651631/c2FtcGxlcy9sb2dv/drilldown"//if no image given
+    },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserModel" }], // Fix ObjectId reference
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserModel" }]  // Fix ObjectId reference
+});
+
+// Export the model
+module.exports = mongoose.model("UserModel", userSchema);
