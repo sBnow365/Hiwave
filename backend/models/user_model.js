716f250e6ee7773150f35860a963d8fc7ba2e0d4
sashwat <sashwatbarthwal@gmail.com>
2025-03-27 23:49:38 +0530

    profile pic made required=true only

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -15,7 +15,8 @@ const userSchema = new mongoose.Schema({
     },
     profilePicUrl: {
         type: String,
-        default: "https://m.media-amazon.com/images/M/MV5BODk3OWIyY2MtM2E0MS00OWYyLTlkNDktMzY4MTE1MDhiYzBiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" // Default profile image
+        default: "https://m.media-amazon.com/images/M/MV5BODk3OWIyY2MtM2E0MS00OWYyLTlkNDktMzY4MTE1MDhiYzBiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", // Default profile image
+        required:true
     },
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserModel" }], // Correct ObjectId reference
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserModel" }]  // Correct ObjectId reference
