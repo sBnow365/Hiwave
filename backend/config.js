9df379e7052ebe0f84193f70770559e9287e1f6b
sashwat <sashwatbarthwal@gmail.com>
2026-07-30 14:20:26 +0530

    removed jwt secret from config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
 module.exports={
     MONGODB_URI:"mongodb+srv://dynamicduo463:dbduoLko@cluster0.ou5l5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    JWT_SECRET:"a;lsjdfl;aksjdf;lka"
+    JWT_SECRET:process.env.JWT_SECRET
 }
